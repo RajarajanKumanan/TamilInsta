2f3b69fbb1da7976d4eeaa7cad83b87ee20d6ffa
Rajarajan <rajarajankumanan@gmail.com>
2019-08-11 14:44:35 +0530

    Tamil Names Initial

--- app/src/main/java/com/socfire/socialfeed/tamilinsta/MainActivity.java
+++ app/src/main/java/com/socfire/socialfeed/tamilinsta/MainActivity.java
@@ -1,38 +1,83 @@
 package com.socfire.socialfeed.tamilinsta;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
-    CardView cdClick;
+import java.util.ArrayList;
+
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
     Toast toastMessage;
+    private AdView mAdView;
+    private CardView boyClick, girlClick, sangamClick, kingClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        cdClick=(CardView)findViewById(R.id.memesIds);
-        cdClick.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showToast("clicked CardView "+v.toString());
-                clickImageView(v);
-            }
-        });
+        setContentView(R.layout.home_screen);
+
+
+        MobileAds.initialize(this,
+                "ca-app-pub-3940256099942544~3347511713");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        boyClick = (CardView) findViewById(R.id.baby_boy);
+        girlClick = (CardView) findViewById(R.id.baby_girl);
+        sangamClick = (CardView) findViewById(R.id.sangam_names);
+        kingClick = (CardView) findViewById(R.id.king_names);
+
+        boyClick.setOnClickListener(this);
+        girlClick.setOnClickListener(this);
+        sangamClick.setOnClickListener(this);
+        kingClick.setOnClickListener(this);
+
+
     }
 
-    private void clickImageView(View v){
-        Intent init =new Intent(this, MemesActivity.class);
-        startActivity(init);
+    @Override
+    public  void onClick(View v){
+
+        switch (v.getId()) {
+            case R.id.baby_boy:
+                showToast("1");
+                break;
+            case R.id.baby_girl:
+                showToast("4");
+                break;
+            case R.id.sangam_names:
+                showToast("3");
+                break;
+            case R.id.king_names:
+                showToast("2");
+                break;
+            default:
+                break;
+        }
     }
 
-    private void showToast(String str){
-        toastMessage.makeText(this,str,Toast.LENGTH_LONG).show();
+
+    private void showToast(String str) {
+        toastMessage.makeText(this, str, Toast.LENGTH_LONG).show();
     }
 
 }
